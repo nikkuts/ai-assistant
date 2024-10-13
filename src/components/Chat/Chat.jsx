09ad5ccc7081f7d3ed2aac98f97244c6a5a29706
Mykola Kuts <nickkuts888@gmail.com>
2024-10-13 23:10:10 +0300

    update Chat

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect  } from 'react';
+import css from './Chat.module.css';
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+  const [isHeader, setIsHeader] = useState(true);
+
+  const textareaRef = useRef(null);
+  const chatBoxRef = useRef(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!input) return;
 
     const newMessage = { text: input, user: 'user' };
-    setMessages([...messages, newMessage]);
+    setMessages([newMessage, ...messages]);
+    setIsHeader(false);
 
     const response = await fetch('https://ai-assistant-backend-05fx.onrender.com/api/chat', {
       method: 'POST',
@@ -20,26 +26,65 @@ const Chat = () => {
     const data = await response.json();
     
     const aiMessage = { text: data.message, user: 'ai' };
-    setMessages([...messages, newMessage, aiMessage]);
+    setMessages([aiMessage, newMessage, ...messages]);
     setInput('');
   };
 
+  const autoResize = () => {
+    const textarea = textareaRef.current;
+    textarea.style.height = 'auto';  
+    textarea.style.height = `${textarea.scrollHeight}px`;  
+  };
+
+  useEffect(() => {
+    autoResize();
+  }, [input]);
+
+  useEffect(() => {
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <div>
-      <div className="chat-box">
-        {messages.map((msg, index) => (
-          <div key={index} className={`message ${msg.user}`}>
+    <div className={css.container}>
+      {isHeader && 
+        <h1>
+          How can I be useful to you?
+        </h1>
+      }
+      <div
+        ref={chatBoxRef}
+        className={css.chatBox}
+      >
+        {messages.slice().reverse().map((msg, index) => (
+          <div
+            key={index}
+            className={`${css.message} ${msg.user === 'user' && css.user}`}
+          >
             {msg.text}
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
+      <form
+        onSubmit={handleSubmit}
+        className={css.form}
+      >
+        <textarea
+          ref={textareaRef}
           type="text"
           value={input}
+          placeholder="Write a message"
+          rows={1}
           onChange={(e) => setInput(e.target.value)}
+          className={css.textarea}
         />
-        <button type="submit">Send</button>
+        <button
+          type="submit"
+          className={css.button}
+        >
+          Send
+        </button>
       </form>
     </div>
   );
